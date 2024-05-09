486f084f2ca4f87daeba3adae11be3b7c19062a4
Mohamed Makhlouf <gaaarw@gmail.com>
2024-05-09 12:39:28 +0300

    Initial Commit

---
+++ SpotifyUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyUI
+//
+//  Created by Mohamed Makhlouf Ahmed on 09/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by Mohamed Makhlouf Ahmed on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
